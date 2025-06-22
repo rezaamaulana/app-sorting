72440b10901c91b8a88274fe4659afc0b61d96b8
rezaamaulana <ezalanamaulana@gmail.com>
2025-06-23 02:36:43 +0800

    update

--- resources/views/dashboard/kuis/IsiKuisInsertion.blade.php
+++ resources/views/dashboard/kuis/IsiKuisInsertion.blade.php
@@ -249,8 +249,8 @@
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title">Hasil {{ implode(' ', explode('-', $materi)) }}</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                aria-label="Close"></button>
+                            {{-- <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button> --}}
                         </div>
                         <div class="modal-body" id="modal-hasil">
 
